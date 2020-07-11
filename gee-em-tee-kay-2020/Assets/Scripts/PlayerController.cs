ce27c256c749d3ce022f72e4b42b1889962be627
rjmcf <rjmcf@live.co.uk>
2020-07-11 21:59:40 +0100

    Remove log

--- gee-em-tee-kay-2020/Assets/Scripts/PlayerController.cs
+++ gee-em-tee-kay-2020/Assets/Scripts/PlayerController.cs
@@ -191,7 +191,6 @@ public class PlayerController : MonoBehaviour
 
     bool CanMoveToValidLocation(int x, int y)
     {
-        Debug.LogFormat("has inhabitant at {0}, {1}: {2}", x, y, Game.worldMap.HasInhabitantAt(x,y));
         return !Game.worldMap.HasInhabitantAt(x,y);
     }
 
