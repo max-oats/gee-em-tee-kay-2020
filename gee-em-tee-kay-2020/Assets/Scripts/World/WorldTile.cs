edebe4b456ef5962d0dc79a6585863c193492b98
beakfriends <mosturnbull@gmail.com>
2020-07-11 19:07:45 +0100

    Added world tiles + bushes and other visual stuff!

--- gee-em-tee-kay-2020/Assets/Scripts/World/WorldTile.cs
+++ gee-em-tee-kay-2020/Assets/Scripts/World/WorldTile.cs
@@ -4,9 +4,48 @@ public class WorldTile : MonoBehaviour
 {
     public int x = 0;
     public int z = 0;
+    
+    public Vector3 rgb;
+    public Vector3 colourOffsets;
+
+    public float bushChance;
+
+    public GameObject bush;
+
+
+    private Color color;
 
     private BaseEntity inhabitant = null;
 
+    void Awake()
+    {
+        SetColour();
+
+        if (Random.value <= bushChance)
+        {
+            GameObject go = Instantiate(bush, 
+                transform.position + new Vector3(Random.Range(-0.4f, 0.4f), 0f, Random.Range(0.3f, 0.4f)), 
+                Quaternion.Euler(new Vector3(0f, Random.Range(0f, 360f), 0f)), transform);
+
+            MaterialPropertyBlock block = new MaterialPropertyBlock();
+            block.SetColor("_Color", color);
+            MeshRenderer debugRenderer = go.GetComponentInChildren<MeshRenderer>();
+            debugRenderer.SetPropertyBlock(block);
+        }
+    }
+
+    void SetColour()
+    {
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+        color = new Color(rgb.x + Random.Range(-colourOffsets.x, colourOffsets.x), 
+                                            rgb.y + Random.Range(-colourOffsets.y, colourOffsets.y), 
+                                            rgb.z + Random.Range(-colourOffsets.z, colourOffsets.z), 1f);
+        block.SetColor("_Color", color);
+                                            
+        MeshRenderer debugRenderer = GetComponentInChildren<MeshRenderer>();
+        debugRenderer.SetPropertyBlock(block);
+    }
+    
     public bool TriggerInteract(InteractParams interactParams)
     {
         if (inhabitant)
@@ -40,9 +79,13 @@ public class WorldTile : MonoBehaviour
 
     public void SetDebugColour(Color inDebugColour)
     {
-        MeshRenderer debugRenderer = GetComponentInChildren<MeshRenderer>();
-        MaterialPropertyBlock block = new MaterialPropertyBlock();
-        block.SetColor("_Color", inDebugColour);
-        debugRenderer.SetPropertyBlock(block);
+        float alpha = 0.5f;
+        if (inDebugColour == Color.white)
+        {
+            alpha = 0f;
+        }
+
+        SpriteRenderer debugRenderer = GetComponentInChildren<SpriteRenderer>();
+        debugRenderer.color = new Color(inDebugColour.r, inDebugColour.g, inDebugColour.b, alpha);
     }
 }
