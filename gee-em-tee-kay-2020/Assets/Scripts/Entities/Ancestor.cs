c3f1a8beb952aa68b95b90818fbc2b09fc89ac54
beakfriends <mosturnbull@gmail.com>
2020-07-12 17:44:46 +0100

    Added "ancestor killing" anim

--- gee-em-tee-kay-2020/Assets/Scripts/Entities/Ancestor.cs
+++ gee-em-tee-kay-2020/Assets/Scripts/Entities/Ancestor.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class Ancestor : BaseEntity
 {
+    public GameObject particleDeath;
+
     public static List<EntityType> obstacleTypes = new List<EntityType>();
 
     [SerializeField]
@@ -15,12 +18,32 @@ public class Ancestor : BaseEntity
             PlayerInteractParams playerParams = interactParams as PlayerInteractParams;
             if (!playerParams.holdingWater && !playerParams.heldAncestor)
             {
-                RemoveFromMap();
+                StartCoroutine(KillAncestor(playerParams));
             }
         }
 
         base.TriggerInteract(interactParams);
 
+    }
+
+    IEnumerator KillAncestor(PlayerInteractParams prms)
+    {
+        PlayerEntity pe = prms.interactingEntity as PlayerEntity;
+        PlayerController pc = pe.GetComponent<PlayerController>();
+        pc.MoveToValidLocation(GetTile().x, GetTile().z);
+
+        yield return new WaitForSeconds(1.0f);
+
+        pc.FaceDirection(Direction.South);
+        pc.GetComponentInChildren<Animator>().CrossFadeInFixedTime("KillAncestor", 0.1f);
+
+        yield return new WaitForSeconds(3f);
+        Game.camera.ScreenShake(1.0f);
+        GameObject go = Instantiate(particleDeath, transform.position, Quaternion.identity);
+        Destroy(go, 5f);
+        yield return new WaitForSeconds(0.3f);
+
+        RemoveFromMap();
     }
 
     public override EntityType GetEntityType()
